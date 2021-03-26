3196e7ba1e2215f13d5e64828166aad43e821a0a
Jason Orense <Jason.Orense@rockwellautomation.com>
2021-03-27 03:41:47 +0800

    added static images, added shoppage component and display data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { Switch, Route, Link  } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   //Route 
@@ -12,7 +13,7 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats/:hatId" component={Homepage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
